faa9b476c0bff43bf272ce6f1a48e64e28ae92b9
Monish C <86337147+Monish0401@users.noreply.github.com>
2025-11-06 15:16:22 +0530

    Header Changes

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,14 +1,48 @@
+// 
+
 import React from "react";
 import "../../App.css";
 
-const Header = () => {
+const Header = ({ theme }) => {
   return (
-    <div className="header">
-        <img src="Space_Applications_Centre_logo.png" alt="SAC Logo" />
-        <h1>Software Defined Radio(SDR) Payload Commanding User Inteface</h1>
-        <img src="Indian_Space_Research_Organisation_Logo.svg.png" alt="ISRO Logo" />
-      </div>
+    <div
+      style={{
+        display: "flex",
+        // CSS properties in style objects must use camelCase
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: "10px 20px",
+      }}
+    >
+      {/* First <img> tag correction: style object must be wrapped in curly braces {} */}
+      <img
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain", // Use camelCase
+        }}
+        src="Space_Applications_Centre_logo.png"
+        alt="SAC Logo"
+      />
+      {/* theme variable must be available, assuming it's passed as a prop */}
+      <h1
+        className={`mb-2 ${theme === "dark" ? "text-white" : "text-gray-900"}`}
+      >
+        Software Defined Radio(SDR) Payload Commanding User Inteface
+      </h1>
+      {/* Second <img> tag correction: style object must be wrapped in curly braces {} 
+          and the property 'style' must be present. */}
+      <img
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain", // Use camelCase
+        }}
+        src="Indian_Space_Research_Organisation_Logo.svg.png"
+        alt="ISRO Logo"
+      />
+    </div>
   );
-}
+};
 
 export default Header;
