74ff9e3f623360bc63b65538c1e7aedf6c213444
Monish C <86337147+Monish0401@users.noreply.github.com>
2025-11-19 06:13:38 +0000

    Update

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -6,19 +6,19 @@ const Footer = ({ theme }) => {
       textAlign: "center",
       padding: "20px",
       // backgroundColor: "#f5f5f5",
-      position: "sticky",
+      position: "static",
       left: 0,
       bottom: 0,
       width: "100%",
       boxShadow: "0 -2px 5px rgba(0,0,0,0.1)"
     }}>
       <h4 className={`mb-2 ${theme === "dark" ? "text-white" : "text-gray-900"}`}>Note: This is not an Official Application. It's still in development....</h4>
-      <code>
-        <b className={`mb-2 ${theme === "dark" ? "text-white" : "text-gray-900"}`} style={{ color: "#000011", padding: "0 10px" }}>
+      <code className={`mb-2 ${theme === "dark" ? "text-white" : "text-gray-900"}`} style={{ padding: "0 10px" }}>
+        <b>
           © Copyright {new Date().getFullYear()} | Designed & Developed by SAC-ISRO.
         </b>
         <br />
-        <b className={`mb-2 ${theme === "dark" ? "text-white" : "text-gray-900"}`} style={{ color: "#000011", padding: "0 10px" }}>
+        <b>
           Space Application Center(Indian Space Research Organisation). All rights reserved.
         </b>
       </code>
